c440705549a4bbb852fcfd944e480b766083c6db
alep0518 <aleksandr.panasenko@netcracker.com>
2020-05-13 20:30:17 +0300

    Added db

--- linkChecker/linkChecker/spiders/dynamic.py
+++ linkChecker/linkChecker/spiders/dynamic.py
@@ -6,6 +6,10 @@ import uuid
 import requests
 from datetime import datetime
 from urllib.parse import urlparse
+from import_to_db import text_to_db
+from import_to_db import image_to_db
+from import_to_db import video_to_db
+import sqlite3
 
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), os.pardir, os.pardir, os.pardir))
 
@@ -53,10 +57,14 @@ while True:
 
 # And grab the page HTML source
 html_page = wd.page_source
-print(html_page)
+# print(html_page)
 wd.quit()
 
 soup = BeautifulSoup(html_page, 'html.parser')
+
+conn = sqlite3.connect("SaTRdatabase.db")
+cursor = conn.cursor()
+vsourse = 'no_video'
 
 textArray = []
 
@@ -67,25 +75,37 @@ for element in soup.find_all('div'):
 text_file = open(os.path.join(save_path, "parsed text.txt"), "w")
 text_file.write("".join(textArray).strip().replace("\t", "").replace("\n", ""))
 text_file.close()
-
+text_to_db("".join(textArray).strip().replace("\t", "").replace("\n", ""), save_path, cursor)
+conn.commit()
+conn.close()
 html_file = open(os.path.join(save_path, "parsed html.txt"), "w")
 html_file.write(html_page)
 html_file.close()
 
 images = soup.findAll('img')
+if os.path.isdir(save_path + "/images") is False:
+    os.mkdir(save_path + "/images")
 for image in images:
     #print image source
-    print('Image:     ' + image['src'])
+    # print('Image:     ' + image['src'])
     filename, file_extension = os.path.splitext(image['src'])
-    print("filename ", file_extension)
+    # print("filename ", file_extension)
     contentData = requests.get(image['src']).content
     unique_filename = str(uuid.uuid4().hex)
-    completeName = os.path.join(save_path, unique_filename + file_extension)
+    completeName = os.path.join(save_path + "/images/", unique_filename + file_extension)
+    print("completeName", completeName)
     with open(completeName, 'wb') as handler:
+        conn = sqlite3.connect("SaTRdatabase.db")
+        cursor = conn.cursor()
+        image_to_db(save_path + "/images", vsourse, cursor)
+        conn.commit()
+        conn.close()
         handler.write(contentData)
 
 
 videos = soup.findAll('video')
+if os.path.isdir(save_path + "/videos") is False:
+    os.mkdir(save_path + "/videos")
 for video in videos:
     #print video source
     print('Image:     ' + video['src'])
@@ -93,6 +113,11 @@ for video in videos:
     print("filename ", file_extension)
     contentData = requests.get(video['src']).content
     unique_filename = str(uuid.uuid4().hex)
-    completeName = os.path.join(save_path, unique_filename + file_extension)
+    completeName = os.path.join(save_path + "/videos/", unique_filename + file_extension)
     with open(completeName, 'wb') as handler:
+        conn = sqlite3.connect("SaTRdatabase.db")
+        cursor = conn.cursor()
+        video_to_db(save_path, vsourse, cursor)
+        conn.commit()
+        conn.close()
         handler.write(contentData)
---
+++ linkChecker/linkChecker/spiders/import_to_db.py
@@ -0,0 +1,63 @@
+import sqlite3
+import os
+
+
+def main():
+    conn = sqlite3.connect("SaTRdatabase.db")
+    cursor = conn.cursor()
+    cursor.execute("""create table if not exists images
+                      (name, path, sourse)""")
+    cursor.execute("""create table if not exists videos
+                      (path)""")
+    cursor.execute("""create table if not exists audio
+                      (path, sourse)""")
+    cursor.execute("""create table if not exists textContent
+                      (text, sourse)""")
+    conn.commit()
+    conn.close()
+
+
+def video_to_db(path, cursor):
+    cursor.execute("""create table if not exists videos
+                   (name, path)""")
+    video = [(str(os.path.basename(path)), str(os.path.abspath(path)))]
+    cursor.executemany("INSERT INTO videos VALUES (?,?)", video)
+
+
+def image_to_db(path, vsourse, cursor):
+    if not str(vsourse) == 'no_video':
+        sourse = str(os.path.abspath(vsourse))
+    else:
+        sourse = vsourse
+    cursor.execute("""create table if not exists images
+                   (name, path, sourse)""")
+    for files in os.listdir(path):
+        # print("files: ", files)
+        # print("path: ", path)
+        image = [(str(files), str(os.path.abspath(path)), str(sourse))]
+        # print("image: ", image)
+        cursor.executemany("INSERT INTO images VALUES (?,?,?)", image)
+
+
+def audio_to_db(path, vsourse, cursor):
+    if not str(vsourse) == 'no_video':
+        sourse = str(os.path.abspath(vsourse))
+    else:
+        sourse = vsourse
+    cursor.execute("""create table if not exists audios
+                   (path, sourse)""")
+    audio = [(str(os.path.abspath(path)), str(sourse))]
+    cursor.executemany("INSERT INTO audios VALUES (?,?)", audio)
+
+
+def text_to_db(text, sourse, cursor):
+    cursor.execute("""create table if not exists textContent
+                   (text, sourse)""")
+    txt = [(str(text), str(os.path.abspath(sourse)))]
+    # print("txt: ", txt)
+    print("text: ", text)
+    cursor.executemany("INSERT INTO textContent VALUES (?,?)", txt)
+
+
+if __name__ == '__main__':
+    main()
